693dc8df74d9a7bb68f2cab7c8ed8f625888cce5
JT <jt@buildingconnected.com>
2018-12-07 10:02:52 -0800

    problem 7

--- problem_01/main.ex
+++ problem_01/main.ex
@@ -1,7 +1,32 @@
 file_contents = File.read! __DIR__ <> "/input.txt"
 
-result = String.split(file_contents)
+numbers = String.split(file_contents)
 	|> Enum.map(&String.to_integer/1)
-	|> Enum.reduce(0, fn num, acc -> num + acc end)
 
-IO.puts Integer.to_string(result)
+# final_sum = Enum.reduce(numbers, 0, fn num, acc -> num + acc end)
+
+defmodule Problem1
+	def hello do
+		IO.puts "hello"
+	end
+end
+
+# frequency = Enum.reduce(
+# 	numbers,
+# 	%{ :current_sum => 0, :map_set => MapSet.new(), :first_repeated_frequency => nil },
+# 	fn number, acc ->
+# 		current_sum = acc[:current_sum] + number
+
+# 		first_repeated_frequency = if acc[:first_repeated_frequency] do
+# 			acc[:first_repeated_frequency]
+# 		else
+# 			acc[:map_set].member?(current_sum) ?
+# 		end
+
+# 		%{ :current_sum => current_sum, :map_set => Map.put(acc[:map_set], :current_sum, current_sum) }
+# 	end
+# )
+
+# IO.puts Integer.to_string(final_sum)
+
+# IO.puts frequency[:current_sum]
---
+++ problem_07/index.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+
+const DATA_REGEX = /Step (\w) must be finished before step (\w) can begin./
+
+class Graph {
+  constructor({ edges }) {
+    this.adjacencyList = new Map()
+    this.inDegrees = {}
+
+    edges.forEach(edge => this.addEdge(edge))
+  }
+
+  addEdge({ v1, v2 }) {
+    if (!this.adjacencyList.has(v1)) {
+      this.adjacencyList.set(v1, [])
+    }
+
+    if (!this.adjacencyList.has(v2)) {
+      this.adjacencyList.set(v2, [])
+    }
+
+    this.adjacencyList.get(v1).push(v2)
+
+    if (!this.inDegrees.hasOwnProperty(v1)) {
+      this.inDegrees[v1] = 0
+    }
+
+    this.inDegrees[v2] = (this.inDegrees[v2] || 0) + 1
+  }
+
+  printDependencyOrdering() {
+    const inDegrees = { ...this.inDegrees }
+
+    while (Object.keys(inDegrees).length) {
+      const first = Object.keys(inDegrees)
+        .filter(key => inDegrees[key] === 0)
+        .sort()[0]
+
+      console.log(first)
+
+      delete inDegrees[first]
+
+      const neighbors = this.adjacencyList.get(first)
+      neighbors.forEach(neighbor => inDegrees[neighbor]--)
+    }
+  }
+}
+
+const parseEdges = line => {
+  const match = line.match(DATA_REGEX)
+
+  const v1 = match[1]
+  const v2 = match[2]
+
+  return { v1, v2 }
+}
+
+const solve = () => {
+  const fileLines = fs
+    .readFileSync(__dirname + '/input.txt', 'utf8')
+    .split('\n')
+    .filter(x => x.trim())
+
+  const edges = fileLines.map(parseEdges)
+
+  const graph = new Graph({ edges })
+
+  graph.printDependencyOrdering()
+}
+
+solve()
