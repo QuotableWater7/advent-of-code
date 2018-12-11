563aaae9684f24b860a348c715301252b7f02501
JT <jt@buildingconnected.com>
2018-12-11 01:12:13 -0800

    remove comments

--- problem_08/index.hs
+++ problem_08/index.hs
@@ -1,6 +1,3 @@
-import System.IO
-import Data.Char
-
 data Node = Null | Leaf [Int] | Parent [Node] [Int] deriving(Show)
 
 extractNChildren :: Int -> [Int] -> ([Node], [Int])
@@ -14,7 +11,6 @@ extractNChildren n list = do
 
   (nodes ++ [node], rest)
 
-
 parseIntoNode :: [Int] -> (Node, [Int])
 parseIntoNode (0:numMetadataValues:xs) = do
   (Leaf (take numMetadataValues xs), drop numMetadataValues xs)
@@ -22,8 +18,7 @@ parseIntoNode [] = (Null, [])
 parseIntoNode (numChildNodes:numMetadataValues:xs) = do
   let (children, rest) = extractNChildren numChildNodes xs
 
-  let metadata_values = take numMetadataValues rest
-  let remaining_input = drop numMetadataValues rest
+  let (metadata_values, remaining_input) = splitAt numMetadataValues rest
 
   ((Parent children metadata_values), remaining_input)
 
@@ -33,17 +28,7 @@ sumOfMetadata (Leaf []) = 0
 sumOfMetadata (Leaf (x:xs)) = x + sumOfMetadata (Leaf xs)
 sumOfMetadata (Parent children metadata_values) = (foldr (+) 0 (map sumOfMetadata children)) + (sumOfMetadata (Leaf metadata_values))
 
--- tokenizeInput :: String -> [Int]
--- tokenizeInput [] = []
--- tokenizeInput (' ':xs) = tokenizeInput xs
--- tokenizeInput (x:xs) = [digitToInt x] ++ (tokenizeInput xs)
-
 main = do
-  -- handle <- openFile "/Users/josephbowler/Documents/practice/advent-of-code/problem_08/input.txt" ReadMode
-  -- contents <- hGetContents handle
-
-  -- print ((filter (/= '\n') contents))
-
   let (node, _) = parseIntoNode [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
 
   print . sumOfMetadata $ node
