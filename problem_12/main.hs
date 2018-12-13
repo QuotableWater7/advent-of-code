5771cd8520219e42ae32527924a9d9ef0d19b227
JT <jt@buildingconnected.com>
2018-12-12 21:28:28 -0800

    comments

--- problem_12/main.hs
+++ problem_12/main.hs
@@ -1,11 +1,14 @@
 data Rule = Rule { configuration :: [Char], next_state :: Char }
 
+-- map over a list, but each iteration of map provides n items from the list
+-- e.g. [1, 2, 3, 4, 5] with chunkSize of 2 provides [1, 2], then [2, 3], then [3, 4], etc
 chunkMap :: Int -> ([a] -> a) -> [a] -> [a]
-chunkMap n fn [] = []
-chunkMap n fn list
-  | (length list) > n = (fn (take n list)) : (chunkMap n fn (tail list))
+chunkMap chunkSize fn [] = []
+chunkMap chunkSize fn list
+  | (length list) > chunkSize = (fn (take chunkSize list)) : (chunkMap chunkSize fn (tail list))
   | otherwise = [fn list]
 
+-- from list of rules, find rule matching our configuration of 5 plants and then apply the next state
 applyMatchingRule :: [Rule] -> [Char] -> Char
 applyMatchingRule [] plants = (head . (drop 2)) plants
 applyMatchingRule ((Rule configuration next_state):rule_tail) plants =
@@ -13,6 +16,8 @@ applyMatchingRule ((Rule configuration next_state):rule_tail) plants =
     then next_state
     else applyMatchingRule rule_tail plants
 
+-- take a list of rules, num_generations to apply, and a full plant configuration
+-- return what that plant configuration looks like in num_generations of applying the rules
 calculateConfiguration :: [Rule] -> Int -> [Char] -> [Char]
 calculateConfiguration rules 0 list = list
 calculateConfiguration rules numGenerations list = do
