f2f3ddfc144fc4a54885fe1ffebf933fb45c18e6
JT <jt@buildingconnected.com>
2018-12-16 20:18:12 -0800

    problem 3

---
+++ problem_03/main.hs
@@ -0,0 +1,20 @@
+import System.IO
+import Data.List
+
+countOccurrences :: Eq a => [a] -> [(a, Int)]
+countOccurrences [] = []
+countOccurrences (x:xs) = do
+  let (matching, rest) = span (\y -> y == x) xs
+
+  [(x, 1 + (length matching))] ++ countOccurrences rest
+
+main = do
+  handle <- openFile "/Users/josephbowler/Documents/practice/advent-of-code/problem_03/input.txt" ReadMode
+  contents <- hGetContents handle
+
+  let tags = words contents
+  let occurrences = map (countOccurrences . sort) tags
+  let with2 = (length . (filter (\x -> length x > 0)) . (map (filter (\(ch, numOccurrences) -> numOccurrences == 2)))) occurrences
+  let with3 = (length . (filter (\x -> length x > 0)) . (map (filter (\(ch, numOccurrences) -> numOccurrences == 3)))) occurrences
+
+  print (with2 * with3)
