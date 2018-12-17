244d050363948a75b8542ddba56e450f2833ce7b
JT <jt@buildingconnected.com>
2018-12-16 21:19:13 -0800

    better solution

--- problem_03/main.hs
+++ problem_03/main.hs
@@ -8,13 +8,17 @@ countOccurrences (x:xs) = do
 
   [(x, 1 + (length matching))] ++ countOccurrences rest
 
+numListsWithCharCount :: Int -> [[(Char, Int)]] -> Int
+numListsWithCharCount n list = (length . (filter (any (\(ch, numOccurrences) -> numOccurrences == n)))) list
+
+checksum :: [[(Char, Int)]] -> Int
+checksum list = (numListsWithCharCount 2 list) * (numListsWithCharCount 3 list)
+
 main = do
   handle <- openFile "/Users/josephbowler/Documents/practice/advent-of-code/problem_03/input.txt" ReadMode
   contents <- hGetContents handle
 
   let tags = words contents
   let occurrences = map (countOccurrences . sort) tags
-  let with2 = (length . (filter (any (\(ch, numOccurrences) -> numOccurrences == 2)))) occurrences
-  let with3 = (length . (filter (any (\(ch, numOccurrences) -> numOccurrences == 3)))) occurrences
 
-  print (with2 * with3)
+  print $ checksum occurrences
